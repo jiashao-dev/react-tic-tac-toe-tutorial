16d5cf2f27d6da223605409f7f25fdd4f14a8498
Jia Shao <jiashao.lau@virtualflex.com.my>
2023-05-09 09:32:12 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,3 +1,42 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import React from "react";
+import { useState } from "react";
+
+export default function Board() {
+  return (
+    <React.Fragment>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+
+      </div>
+      <div className="board-row">
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button 
+      className="square" 
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  )
 }
