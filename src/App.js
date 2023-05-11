80a45971be3358b7dc484cdb64ba4cb53d433707
Jia Shao <jiashao.lau@virtualflex.com.my>
2023-05-11 09:43:44 +0800

    Allow to track history

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+// Game component
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+  const xIsNext = currentMove % 2 === 0;
+
+  const moves = history.map((squares, move) => {
+    let description = move > 0 ? "Go to move #" + move : "Go to game start";
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    )
+  })
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+// Board component
+function Board({ xIsNext, squares, onPlay }) {
   const winner = calculateWinner(squares);
 
   let status = winner ? "Winner: " + winner : "Next player: " + (xIsNext ? "X" : "O");
@@ -15,8 +51,7 @@ export default function Board() {
 
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? "X" : "O";
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   return (
@@ -41,6 +76,7 @@ export default function Board() {
   );
 }
 
+// Square children component
 function Square({ value, onSquareClick }) {
   return <button className="square" onClick={onSquareClick}>{value}</button>
 }
